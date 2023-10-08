f84ab6ef1b2439f2c93277a05d5cda1f58d039b7
Christoph Giesche <christoph.giesche@cgidev.de>
2023-10-08 21:09:28 +0200

    Fixed eslint problems

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const path = require('path')
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
